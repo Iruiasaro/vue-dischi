fececfc0707b9e4ad4e8c73f0766530a1f6a9ca6
Iruiasaro <brunettirosario@gmail.com>
2021-05-13 09:27:29 +0200

    ordina per genere musicale

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Attraverso una chiamata ajax all’API di boolean https://flynn.boolean.careers/exercises/api/array/music avremo a disposizione una decina di dischi musicali. Utilizzando vue, stampiamo a schermo una card per ogni album.
 
+// //BONUS:
+// Creare una select con tutti i generi dei dischi. In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
+
+//BONUS 2: Ordinare i dischi per anno di uscita.
+
 var app = new Vue({
     el: "#app",
 
@@ -7,45 +12,58 @@ var app = new Vue({
         albumList: [],
         filter: 'filtro',
         newAlbum: [],
-        oldAlbum: []
+        oldAlbum: [],
+        tipologia: ['All'],
+        selected: 'All',
     },
 
     methods: {
-        
+        currentGenre: function () {
+            if (this.selected == 'All') {
+                return '';
+            } else {
+                return this.selected;
+            }
+        }
     },
 
     mounted() {
         axios.get("https://flynn.boolean.careers/exercises/api/array/music")
             .then((resp) => {
+
                 this.albumList = resp.data.response;
+
+                this.albumList.forEach((item, i) => {
+
+                    if (!this.tipologia.includes(item.genre)) {
+
+                        this.tipologia.push(item.genre);
+                    }
+                });
             });
     },
 
     computed: {
         albumSort: function () {
-          if (this.filter == 'ancient') {
+            if (this.filter == 'ancient') {
 
-            this.newAlbum = [...this.albumList];
-            this.newAlbum.sort(function(x,y) 
+                this.newAlbum = [...this.albumList];
+                this.newAlbum.sort(function (x, y) { return x.year - y.year; })
 
-            {return x.year - y.year;})
+                return this.newAlbum;
 
-            return this.newAlbum;
+            } else if (this.filter == 'young') {
 
-          } else if (this.filter == 'young') {
+                this.oldAlbum = [...this.albumList];
 
-            this.oldAlbum = [...this.albumList];
+                this.oldAlbum.sort(function (x, y) { return y.year - x.year; })
 
-            this.oldAlbum.sort(function(x,y) 
+                return this.oldAlbum;
 
-            {return y.year - x.year;})
+            } else {
 
-            return this.oldAlbum;
-            
-          } else {
-              
-            return this.albumList;
-          }
+                return this.albumList;
+            }
         }
     }
 });
